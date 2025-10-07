f515100c5ac7b3cf82949e4a9e982699cd439501
xnjwithwd <42474177+xnjwithwd@users.noreply.github.com>
2025-10-07 14:58:43 +0800

    Update ubuntu_install_ssr.sh

--- ubuntu_install_ssr.sh
+++ ubuntu_install_ssr.sh
@@ -13,11 +13,11 @@ V6_PROXY=""
 IP=`curl -sL -4 ip.sb`
 if [[ "$?" != "0" ]]; then
     IP=`curl -sL -6 ip.sb`
-    V6_PROXY="https://gh.hijk.art/"
+    V6_PROXY=""
 fi
 
 FILENAME="ShadowsocksR-v3.2.2"
-URL="${V6_PROXY}https://github.com/shadowsocksrr/shadowsocksr/archive/3.2.2.tar.gz"
+URL="https://github.com/shadowsocksrr/shadowsocksr/archive/3.2.2.tar.gz"
 BASE=`pwd`
 OS=`hostnamectl | grep -i system | cut -d: -f2`
 
